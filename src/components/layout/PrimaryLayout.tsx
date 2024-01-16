93c5b21c3fd0a53cd19c2691ec63559a9c3b7923
Yuma Satake <satous.y@icloud.com>
2024-01-16 15:05:06 +0900

    feat: Home・Detail Page 追加

---
+++ src/components/layout/PrimaryLayout.tsx
@@ -0,0 +1,18 @@
+import { Container } from "@mui/material";
+import { FC, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+/**
+ * Primary Layout
+ */
+export const PrimaryLayout: FC<Props> = ({ children }) => {
+  return (
+    <Container maxWidth='lg'>
+      PrimaryLayout
+      {children}
+    </Container>
+  );
+};
