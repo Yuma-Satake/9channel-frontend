8077d1de0f7bdf10aa2d8c71fc7559d2e3c8ba70
Yuma Satake <satous.y@icloud.com>
2024-01-26 12:47:42 +0900

    Add thread and reply functionality to DetailPage

--- src/components/pages/DetailPage.tsx
+++ src/components/pages/DetailPage.tsx
@@ -1,7 +1,9 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Button, Stack, TextField } from '@mui/material';
 import { PrimaryLayout } from '../layout/PrimaryLayout';
 import Logo from '../../assets/logo.png';
+import { ReplyItem, ThreadItem } from './HomePage';
+import { axiosBase } from '../lib/axiosUtil';
 
 /**
  * Detail Page
@@ -10,9 +12,58 @@ export const DetailPage: FC = () => {
   const query = new URLSearchParams(window.location.search);
   const threadId = query.get('threadId');
 
-  const [name, setName] = useState('');
+  const [thread, setThread] = useState<ThreadItem | null>(null);
+  const [reply, setReply] = useState<ReplyItem[]>([]);
+
+  // const [name, setName] = useState('');
   const [text, setText] = useState('');
 
+  const updateReply = async () => {
+    const res = await axiosBase.get('/reply', {
+      params: {
+        thread_id: threadId,
+      },
+    });
+    setReply(res.data.reply);
+  };
+
+  const onReplySubmit = async () => {
+    await axiosBase.post('/createReply', {
+      params: {
+        thread_id: threadId,
+        body: text,
+      },
+    });
+
+    setText('');
+
+    // スレッドを再読み
+    await updateReply();
+  };
+
+  useEffect(() => {
+    if (threadId !== null) {
+      (async () => {
+        const threadPromise = axiosBase.get('/thread', {
+          params: {
+            thread_id: threadId,
+          },
+        });
+
+        const replyPromise = axiosBase.get('/reply', {
+          params: {
+            thread_id: threadId,
+          },
+        });
+
+        const [threadRes, replyRes] = await Promise.all([threadPromise, replyPromise]);
+        setThread(threadRes.data.thread);
+        setReply(replyRes.data.reply);
+      })();
+    }
+  }, []);
+
+  if (threadId === null || thread === null) return <h1>読み込み中です</h1>;
   return (
     <PrimaryLayout>
       <div
@@ -36,13 +87,17 @@ export const DetailPage: FC = () => {
             padding: '5px',
           }}
         >
-          <a href="/">ホーム</a>
-          <a href="/list">板一覧へ</a>
-          <a href="/list">ログイン</a>
+          <a href="/" style={{ textDecoration: 'none' }}>
+            ホーム
+          </a>
+          <a href="/" style={{ textDecoration: 'none' }}>
+            板一覧へ
+          </a>
+          <a href="/" style={{ textDecoration: 'none' }}>
+            ログイン
+          </a>
         </Stack>
-        <h1>
-          松本人志週刊文春提訴賠償請求額は約５億５千万円報道から２６日－芸人人生かけ動き出す一方、元タレントが顔出し告発
-        </h1>
+        <h1>{thread.thread_title}</h1>
         <br />
         <div
           style={{
@@ -53,7 +108,6 @@ export const DetailPage: FC = () => {
         >
           <Stack direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={4}>
             <div>▼0001</div>
-
             <div
               style={{
                 color: '#9400d3',
@@ -88,45 +142,47 @@ export const DetailPage: FC = () => {
               borderTop: '1px solid #e6e6fa',
             }}
           >
-            お笑いコンビ・ダウンタウンの松本人志（６０）が２２日、女性を巡る自身に対する報道で名誉を毀損（きそん）されたとして、「週刊文春」の発行元である文芸春秋などを相手に損害賠償などを求める訴えを起こした。所属の吉本興業が発表した。松本個人による提訴となる。代理人によると損害賠償の請求額は約５億５千万円。文芸春秋側は「一連の記事には十分に自信を持っています」と声明を発表した。お笑い界のトップに君臨してきた松本を巡る騒動が動き出した。
-            衝撃の文春砲から２６日目。松本が動いた。この日、所属の吉本興業が「当社所属タレント松本人志に関するお知らせ」と題した文書を公表し文芸春秋社などを提訴したと明らかにした。
-            松本の代理人を務める田代政弘弁護士はコメントを発表。「本日、松本人志氏は、株式会社文芸春秋ほか１名に対して、令和５年１２月２７日発売の週刊文春に掲載された記事に関し、名誉毀損に基づく損害賠償請求及び訂正記事による名誉回復請求を求める訴訟を提起いたしました」とした。文芸春秋に求めている損害賠償の請求額は約５億５千万円という。
-            松本をめぐっては、週刊文春および、文春オンラインが、２０１５年に都内のホテルで複数の女性に対して性的行為を強要したなどとする記事を掲載していた。
-            松本の代理人弁護士は「今後、裁判において、記事に掲載されているような性的行為やそれらを強要した事実はなく、およそ『性加害』に該当するような事実はないということを明確に主張し立証してまいりたいと考えております」と表明。文春の記事の真実性を全面的に争う姿勢を見せた。
-            文春の報道をめぐっては、発売当日に吉本興業が即座に反応。「当該事実は一切なく、タレントの社会的評価を著しく低下させ、名誉を毀損するもので、法的措置を検討していく」と否定し訴訟の提起を示唆していた。
-            その後、松本は１月８日に活動休止を発表。「裁判との同時並行では、これまでのようにお笑いに全力を傾けることができなくなってしまうため、活動を休止したい」との意向を吉本に伝え、自身も「事実無根なので闘いまーす。」とＸに投稿。全面対決の姿勢を鮮明にしていたが、それを実行に移す形となった。
-            お笑い界のトップに君臨してきた松本だったが、報道により７本のレギュラー番組から姿を消し、名誉も失った。少なくとも２年は要すると言われる裁判。芸人人生をかけた長く苦しい闘いの行方に注目が集まる。
+            {thread.thread_content}
           </div>
         </div>
-        <div
-          style={{
-            fontSize: '20px',
-            display: 'block',
-            border: '1px solid #e6e6fa',
-            backgroundColor: '#0000;',
-          }}
-        >
-          ▼0002 名前: 投稿者2
-          <Stack
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="flex-end"
-            spacing={8}
-            style={{
-              fontSize: '18px',
-            }}
-          >
-            投稿日: 2021/10/01 12:00:00ID: userID
-          </Stack>
-          <div
-            style={{
-              fontSize: '23px',
-              display: 'block',
-            }}
-          >
-            なんこれ？？
-          </div>
-        </div>
+        {reply.map((reply, index) => {
+          const replyNum = index + 2;
+
+          return (
+            <div
+              key={reply.reply_id}
+              style={{
+                fontSize: '20px',
+                display: 'block',
+                border: '1px solid #e6e6fa',
+                backgroundColor: '#0000;',
+              }}
+            >
+              {/* ▼0002 名前: 名無しさん */}
+              {`${replyNum} 名前: 名無しさん`}
+              <Stack
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+                spacing={8}
+                style={{
+                  fontSize: '18px',
+                }}
+              >
+                {`投稿日: ${reply.created_at} ID: ${reply.reply_id}`}
+              </Stack>
+              <div
+                style={{
+                  fontSize: '23px',
+                  display: 'block',
+                }}
+              >
+                {reply.body}
+              </div>
+            </div>
+          );
+        })}
+
         <div
           style={{
             borderTop: '1px solid #e6e6fa',
@@ -135,14 +191,12 @@ export const DetailPage: FC = () => {
         >
           <Button
             variant="text"
-            onClick={() => {
-              alert('新着レスを表示します');
-            }}
+            onClick={updateReply}
             style={{
               fontSize: '25px',
             }}
           >
-            新着レスを表示
+            レスを更新
           </Button>
         </div>
         <div
@@ -154,9 +208,10 @@ export const DetailPage: FC = () => {
         >
           <h2>リプライする</h2>
           <Stack direction="column" justifyContent="flex-start" alignItems="flex-start" spacing={4}>
-            <TextField value={name} label="name" onChange={(e) => setName(e.target.value)} />
+            {/* <TextField value={name} label="name" onChange={(e) => setName(e.target.value)} /> */}
             <TextField
               value={text}
+              fullWidth
               id="filled-multiline-static"
               multiline
               rows={4}
@@ -166,10 +221,8 @@ export const DetailPage: FC = () => {
           </Stack>
           <br />
           <Button
-            variant="text"
-            onClick={() => {
-              alert('投稿します');
-            }}
+            variant="contained"
+            onClick={onReplySubmit}
             style={{
               alignItems: 'center',
               fontSize: '25px',
@@ -191,7 +244,9 @@ export const DetailPage: FC = () => {
               fontSize: '25px',
             }}
           >
-            <a href="#">一番上に移動</a>
+            <a href="#" style={{ textDecoration: 'none' }}>
+              一番上に移動
+            </a>
           </Button>
         </div>
         <Stack
